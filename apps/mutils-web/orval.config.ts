8fed8070d7325d511150b33205bda571e436220c
Shiron <contact@shiron.io>
2026-03-27 17:57:18 +0100

    feat(mutils-web): Added Orval for API client generation

---
+++ apps/mutils-web/orval.config.ts
@@ -0,0 +1,18 @@
+﻿import { defineConfig } from 'orval';
+
+export default defineConfig({
+    mutils: {
+        output: {
+            mode: 'tags-split',
+            target: 'src/api/mutils.ts',
+            schemas: 'src/api/model',
+            client: 'react-query',
+            mock: true,
+            httpClient: 'axios',
+            biome: true
+        },
+        input: {
+            target: 'http://127.0.0.1:1810/openapi/v1.json',
+        },
+    },
+});
