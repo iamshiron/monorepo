8915cb37400cdaf39cf535c21d588b738adfdf83
Shiron <contact@shiron.io>
2026-03-27 09:57:20 +0100

    chore: Stopped tracking .env file

---
+++ backend/src/BeatDash.API/Data/BeatDashSeeder.cs
@@ -0,0 +1,7 @@
+﻿namespace Shiron.BeatDash.API.Data;
+
+public static class Seeder {
+    public static void SeedMockData(this BeatDashDbContext context) {
+        if (context.PlaySessions.Any()) return;
+    }
+}
