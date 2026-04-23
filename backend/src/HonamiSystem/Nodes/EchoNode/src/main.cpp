b1fa78b9dac0e06d7b232d790b9bf1ecf153099e
Shiron <contact@shiron.io>
2026-04-23 20:19:58 +0200

    build: Added Nx target caching, clean targets, and gRPC integration for EchoNode

--- backend/src/HonamiSystem/Nodes/EchoNode/src/main.cpp
+++ backend/src/HonamiSystem/Nodes/EchoNode/src/main.cpp
@@ -1,6 +1,9 @@
+#include <grpcpp/grpcpp.h>
 #include <iostream>
 
 int main(int argc, char** argv) {
-	std::cout << "Hello, EchoNode!" << std::endl;
+	std::string grpc_version = grpc::Version();
+	std::cout << "Using gRPC " << grpc_version << std::endl;
+
 	return 0;
 }
